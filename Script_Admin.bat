493b56f5f30bc4e91b70c5130b2efe20920e928e
Rafa <74628132+rarodja@users.noreply.github.com>
2023-05-02 02:28:23 +0200

    Add files via upload

---
+++ Script_Admin.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsAlarms* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.XboxGamingOverlay* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsCamera* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsFeedbackHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.People* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.BingWeather* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsSoundRecorder* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.WindowsMaps* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.GetHelp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Office.OneNote* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MSPaint* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.MixedReality.Portal* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Getstarted* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.YourPhone* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.Microsoft3DViewer* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.XboxApp* | Remove-AppxPackage"
+PowerShell -Command "Get-AppxPackage *Microsoft.549981C3F5F10* | Remove-AppxPackage"
+%WINDIR%\SysWOW64\OneDriveSetup.exe /Uninstall
+RMDIR /S /Q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\Administrative Tools"
+RMDIR /S /Q "%PROGRAMDATA%\Microsoft\Windows\Start Menu\Programs\System Tools"
+RMDIR /S /Q "%APPDATA%\Microsoft\Windows\Start Menu\Programs\System Tools"
+RMDIR /S /Q %ONEDRIVE%
+PAUSE
+EXIT
